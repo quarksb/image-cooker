485184f09e828bf777463f7f80932de54b022ebd
kuake <kuake@gaoding.com>
2022-02-12 22:44:35 +0800

    deploy

--- assets/index.dd19aa5c.js
+++ assets/index.dd19aa5c.js
@@ -6,26 +6,25 @@ import{t as se}from"./vendor.eab43d9c.js";const le=function(){const e=document.c
     [[location(0)]] fragUV: vec2<f32>;
 };
 
-let pos = array<vec2<f32>, 6>(
-    vec2<f32>(1.0, 1.0),
-    vec2<f32>(1.0, -1.0),
-    vec2<f32>(-1.0, -1.0),
-    vec2<f32>(1.0, 1.0),
-    vec2<f32>(-1.0, -1.0),
-    vec2<f32>(-1.0, 1.0)
-);
-
-let uv = array<vec2<f32>, 6>(
-    vec2<f32>(1.0, 0.0),
-    vec2<f32>(1.0, 1.0),
-    vec2<f32>(0.0, 1.0),
-    vec2<f32>(1.0, 0.0),
-    vec2<f32>(0.0, 1.0),
-    vec2<f32>(0.0, 0.0)
-);
-
 [[stage(vertex)]]
 fn vert_main([[builtin(vertex_index)]] VertexIndex: u32) -> VertexOutput {
+    let pos = array<vec2<f32>, 6>(
+        vec2<f32>(1.0, 1.0),
+        vec2<f32>(1.0, -1.0),
+        vec2<f32>(-1.0, -1.0),
+        vec2<f32>(1.0, 1.0),
+        vec2<f32>(-1.0, -1.0),
+        vec2<f32>(-1.0, 1.0)
+    );
+
+    let uv = array<vec2<f32>, 6>(
+        vec2<f32>(1.0, 0.0),
+        vec2<f32>(1.0, 1.0),
+        vec2<f32>(0.0, 1.0),
+        vec2<f32>(1.0, 0.0),
+        vec2<f32>(0.0, 1.0),
+        vec2<f32>(0.0, 0.0)
+    );
     var output: VertexOutput;
     output.Position = vec4<f32>(pos[VertexIndex], 0.0, 1.0);
     output.fragUV = uv[VertexIndex];
